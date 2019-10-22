35481286345e064b71d506904ceb65d6fcc73000
AcediaMiyh <56843635+AcediaMiyh@users.noreply.github.com>
2019-10-22 11:15:45 +0500

    Update js.js

--- JS/js.js
+++ JS/js.js
@@ -9,4 +9,15 @@ window.onload = function() {
     }
     slide.style.left = i + "px";
   }
+  var click = 0;
+  document.getElementById("button").onclick = function() {
+    if (click == 0) {
+      document.getElementById("text").append("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae temporibus ullam perspiciatis laudantium! Possimus nemo eius accusantium nesciunt laborum autem aperiam necessitatibus fuga. Cumque, magni. Deleniti repellendus ipsum repudiandae expedita.Officia necessitatibus sed illo beatae, deleniti nobis earum dicta blanditiis molestiae. Quae illo quis aperiam delectus natus adipisci, nulla suscipit ipsum architecto, impedit cupiditate! Nihil dolor explicabo tempore aliquam quod.Voluptatum nulla quis, mollitia numquam totam minima labore eveniet libero laborum soluta nostrum exercitationem voluptate sit officia nesciunt suscipit quos ullam assumenda cupiditate. Doloribus quos qui cum dignissimos atque iure!");
+      click = 1;
+    }
+    else{
+      document.getElementById("text").innerHTML = "";
+      click = 0;
+    }
+  };
 };
